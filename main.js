902d85bc22aa0ff739c44c075a4d20a112dd908e
damjanpavlica <44097267+damjanpavlica@users.noreply.github.com>
2018-11-06 19:42:56 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+/* SELEKTORI */
+
+const prvi = document.getElementById('prvi')
+const drugi = document.getElementById('drugi')
+const treci = document.getElementById('treci')
+const cetvrti = document.getElementById('cetvrti')
+const peti = document.getElementById('peti')
+const sesti = document.getElementById('sesti')
+
+/* DOGAĐAJI */
+
+prvi.addEventListener('click', function() {
+    prvi.style.backgroundColor = 'orange'
+})
+
+prvi.addEventListener('dblclick', function() {
+    prvi.style.backgroundColor = 'pink'
+})
+
+drugi.addEventListener('dblclick', function() {
+    drugi.innerText = 'Hvala!'
+})
+
+treci.addEventListener('mouseover', function() {
+    treci.style.backgroundColor = 'crimson'
+})
+
+cetvrti.addEventListener('mouseout', function() {
+    cetvrti.style.backgroundColor = 'white'
+    cetvrti.style.color = 'black'
+    cetvrti.innerText = 'Dovidjenja'
+})
+
+peti.addEventListener('mousemove', function(e) {
+    peti.innerText = 'Koordinate misa x: ' + e.clientX + ' y: ' + e.clientY
+})
+
+sesti.addEventListener('click', function() {
+    prvi.style.display = 'none'
+})
+
+sesti.addEventListener('dblclick', function() {
+    prvi.style.display = 'inline-block'
+})
